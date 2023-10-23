f570c7e787035770c0e3080f2b05e803c15bbf9e
James <118614403+JumesP@users.noreply.github.com>
2023-10-24 00:26:29 +0100

    broken db

--- mongo.js
+++ mongo.js
@@ -13,8 +13,13 @@ async function main() {
             summary: "A charming loft in Paris",
             bedrooms: 1,
             bathrooms: 2
-        })
+        });
         await findOneListingByName(client, "Lovely loft");
+        await findListingWithMinimumBedroomsBathroomsAndMostRecentReviews(client, {
+            minimumNumberOfBedrooms: 4,
+            minimumNumberOfBathrooms: 2,
+            maximumNumberOfResults: 5
+        });
 
     } catch (error) {
         console.error(error);
@@ -34,15 +39,44 @@ async function createListing(client, newListing){
 }
 
 // read listings
+async function findListingWithMinimumBedroomsBathroomsAndMostRecentReviews(client, {
+    minimumNumberOfBedrooms = 0,
+    minimumNumberOfBathrooms = 0,
+    maximumNumberOfResults = Number.MAX_SAFE_INTEGER
+} = {}){
+
+    const cursor = client.db("sample_airbnb").collection("listingsAndReviews").find({
+        bedrooms: {$gte: minimumNumberOfBedrooms},
+        bathrooms: {$gte: minimumNumberOfBathrooms}, // for functions on mongodbs docs
+    }).sort({ last_review: -1 })
+        .limit(maximumNumberOfResults);
+    const results = await cursor.toArray();
+
+    if (results.length > 0) {
+        console.log(`Found listing(s) with at least ${minimumNumberOfBedrooms} bedrooms and ${minimumNumberOfBathrooms} bathrooms: `)
+        results.forEach((result, i) => {
+            let date = new Date(result.last_review).toDateString();
+            console.log();
+            console.log(`${i + 1}. name: ${result.name}`);
+            console.log(`   _id: ${result._id}`);
+            console.log(`   bedrooms: ${result.bedrooms}`);
+            console.log(`   bathrooms: ${result.bathrooms}`);
+            console.log(`   most recent review date: ${new Date(result.last_review)}`);
+
+        })
+    }
+}
+
 async function findOneListingByName(client, nameOfListing) {
-    client.db("sample_airbnb").collection("listingsAndReviews").findOne({
+    const result = await client.db("sample_airbnb").collection("listingsAndReviews").findOne({
         name: nameOfListing
     })
 
     if (result) {
-        console.log("found a listing in the collection with the name ${nameOfListing}")
+        console.log(`found a listing in the collection with the name "${nameOfListing}"`)
+        console.log(result)
     } else {
-        console.log("No listings found with that name")
+        console.log(`No listings found with that name`)
     }
 }
 
