4bee0b551053e7e4aee2cc401b31758a8def21d4
James <118614403+JumesP@users.noreply.github.com>
2024-04-24 14:55:26 +0100

    more changes

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.set("view engine", "ejs");
 
 app.use(fileUpload());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "/public")));
+app.use("/", express.static(path.join(__dirname, "/public")));
 app.use("/css", express.static(path.join(__dirname, "/node_modules/bootstrap/dist/css")));
 app.use("/js", express.static(path.join(__dirname, "/node_modules/bootstrap/dist/js")));
 app.use("/js", express.static(path.join(__dirname, "/node_modules/jquery/dist")));
